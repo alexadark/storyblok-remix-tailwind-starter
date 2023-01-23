5b4fde7665ff7f1877e5ddc6b3138787d3775ded
Alexa spalato <alexaspalato@gmail.com>
2023-01-23 09:01:22 +0100

    fix tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   plugins: [
-    "postcss-import",
-    "tailwindcss/nesting",
-    "tailwindcss",
-    "autoprefixer",
+    require("postcss-import"),
+    require("tailwindcss/nesting"),
+    require("tailwindcss"),
+    require("autoprefixer"),
   ],
-}
-
+};
